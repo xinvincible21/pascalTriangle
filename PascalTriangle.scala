7df1eec3b4486582f293a9c0271765dfcd2e3a9a
Ryan Douglass <ryan.douglass@medialets.com>
2017-03-11 19:28:38 -0500

    init

---
+++ PascalTriangle.scala
@@ -0,0 +1,26 @@
+
+
+def currentRow(previousRow:IndexedSeq[Int]) = {
+    for (index <- 0 to previousRow.length) yield {
+        if(index == 0 || index == previousRow.length){
+            1
+        }else{
+            previousRow(index - 1) + previousRow(index)
+        }
+
+    }
+}
+
+def pascalTriangle(n:Int) = {
+    var rows = IndexedSeq(IndexedSeq(1), IndexedSeq(1, 1))
+    for (index <- 1 to n) yield {
+        if (index == 1) {
+            rows(index)
+        }
+        else {
+            val previousRow = currentRow(previousRow = rows(index - 1))
+            rows = rows ++ IndexedSeq(previousRow)
+        }
+    }
+     rows
+}
